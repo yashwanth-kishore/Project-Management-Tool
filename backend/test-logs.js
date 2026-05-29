57ed083f892e98dea4d0d6f1dbf92bec5ee366c2
Yashwanth Kishore <singamyashwanthkishore@gmail.com>
2026-05-29 18:45:06 +0530

    Remove temporary files

--- backend/test-logs.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./src/db');
-pool.query('SELECT * FROM activity_logs ORDER BY created_at DESC LIMIT 5').then(res => {
-  console.log(JSON.stringify(res.rows, null, 2));
-  process.exit(0);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
