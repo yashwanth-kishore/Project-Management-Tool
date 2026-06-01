0de78a42c7ae1b46919c371de4a95c30e82e82c1
Yashwanth Kishore <singamyashwanthkishore@gmail.com>
2026-06-01 18:57:30 +0530

    Cleanup: removed unused backend scripts

--- backend/inspect-activity.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const pool = require('./src/db');
-pool.query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'activity_logs'")
-  .then(res => { fs.writeFileSync('schema.json', JSON.stringify(res.rows, null, 2)); process.exit(0); })
-  .catch(err => { console.error(err); process.exit(1); });
